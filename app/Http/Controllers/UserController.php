a6d66e7c083aa529218c3a37e8a5bdd08defedf5
Connor Early <connor-early@uiowa.edu>
2017-07-29 17:03:48 -0500

    Made some changes to the update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -73,6 +73,7 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $user = User::find($id);
         return view('pages.user-edit', $user);
     }
 
