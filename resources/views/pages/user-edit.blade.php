2e65da6a742cf69e4182cdbc7df20974956aba79
Joshua Cottrell <joshua.e.cottrell@gmail.com>
2017-07-29 02:58:44 -0500

    Added User model back; got user listings and pages

--- resources/views/pages/user-edit.blade.php
+++ resources/views/pages/user-edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-10 col-md-10 col-lg-offset-1 col-md-offset-1">
         @if(! isset($id)) Create user @else Edit user @endif
         <form class="form" method="POST" action="/user/{{ $id }}">
         </form>
--- resources/views/pages/user-list.blade.php
+++ resources/views/pages/user-list.blade.php
@@ -1,23 +1,24 @@
 @extends('layout')
 
 @section('content')
-    Users
-    <table class="col-lg-8 table table-striped table-bordered">
-        <thead>
+    <div class="col-lg-10 col-md-10 col-lg-offset-1 col-md-offset-1">
+        <table class="col-lg-8 table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <td>Id</td>
+                    <td>Name</td>
+                    <td>Description</td>
+                </tr>
+            </thead>
+            <tbody>
+        @foreach($users as $user)
             <tr>
-                <td>Id</td>
-                <td>Name</td>
-                <td>Description</td>
+                <td><a href="/users/{{$user->id}}/edit" >{{$user->id}}</a></td>
+                <td><a href="/users/{{$user->id}}">{{$user->name}}</a></td>
+                <td>{{$user->description}}</td>
             </tr>
-        </thead>
-        <tbody>
-    @foreach($users as $user)
-        <tr>
-            <td><a href="/users/{{$user->id}}/edit" >{{$user->id}}</a></td>
-            <td><a href="/users/{{$user->id}}">{{$user->name}}</a></td>
-            <td>{{$user->description}}</td>
-        </tr>
-    @endforeach
-        </tbody>
-    </table>
+        @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
