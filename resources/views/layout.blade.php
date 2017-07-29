c43f525672c9bfb902952e2603770856c36027bf
Steven Peercy <speercy@gmail.com>
2017-07-29 00:09:13 -0400

    Adding front-end

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Alia | Places stay for missionaries</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300,400,600">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/css/app.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <a href="/"><img class="logo" src="/assets/img/alia.png" alt="Alia"></a>
+            <nav class="pull-right">
+                <a href="/"><i class="fa fa-home"></i> Home</a>
+                <a href="/"><i class="fa fa-hotel"></i> Listings</a>
+                <a href="/"><i class="fa fa-user-circle-o"></i> Account</a>
+            </nav>
+        </div>
+    </header>
+
+    @yield('content')
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
 Route::resource('users', 'UserController');
 Route::resource('listings', 'ListingController');
