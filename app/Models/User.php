31f00f2391c70d1c5071a6dbf00ff09881caaa00
Joshua Cottrell <joshua.e.cottrell@gmail.com>
2017-07-29 10:01:59 -0500

    Moved User model to previously created one; created Listing model

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Models\Church');
     }
+
+    public function listings()
+    {
+        return $this->hasMany('App\Models\Listing');
+    }
 }
