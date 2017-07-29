5eb1135c0e6c9c5806ff3fa669763023d9824d42
Steven Peercy <speercy@gmail.com>
2017-07-29 12:07:13 -0400

    Adds Listing index

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Listing;
 
 class ListingController extends Controller
 {
@@ -13,7 +14,7 @@ class ListingController extends Controller
      */
     public function index()
     {
-        return view('pages.listing-list');
+        return view('pages.listing-list', [ 'listings' => Listing::all() ]);
     }
 
     /**
