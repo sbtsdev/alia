8037352a6d623ee74b9cd4ef6eeaf96a40296e12
rbayer <rbayer@sbts.edu>
2017-07-28 22:36:41 -0500

    add basic migration

--- database/migrations/2014_10_12_000000_create_churches_table.php
+++ database/migrations/2014_10_12_000000_create_churches_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateChurchesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('churches', function (Blueprint $table) {
+            $table->increments('churches_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
-            $table->string('description');
+            $table->string('phone',13); //13 for country code
             $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('churches');
     }
 }
