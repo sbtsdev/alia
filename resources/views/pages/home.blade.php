8aef83e9e63b6919d77cf5ca5ef68a030d324212
Steven Peercy <speercy@gmail.com>
2017-07-29 01:25:14 -0400

    Front end updates

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layout')
 
 @section('content')
-    Home
+    <div class="hero">
+        <div class="container">
+            <div class="col-lg-6 col-lg-offset-3">
+                <div class="input-group search">
+                    <input type="text" class="form-control input-lg" placeholder="Find a city&hellip;">
+                    <span class="input-group-btn">
+                        <button class="btn btn-lg btn-coral" type="button">Search</button>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
